0d4062427deca0ee35d06c4f0edd7fdf574a1c21
ulrokx <92767187+ulrokx@users.noreply.github.com>
2022-01-17 17:06:50 -0500

    removed logs

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -22,7 +22,6 @@ const Login: React.FC = ({}) => {
             <Formik
                 initialValues={{ usernameOrEmail: "", password: "" }}
                 onSubmit={async (values, { setErrors }) => {
-                    console.log(values);
                     const response = await login(values);
                     if (response.data?.login.errors) {
                         setErrors(toErrorMap(response.data.login.errors));
